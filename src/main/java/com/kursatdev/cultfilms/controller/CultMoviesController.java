cf1766948d9b2a74b129bb82895207c405ee063c
kursatsmsek <kursatsimsek@protonmail.ch>
2023-03-24 15:54:46 +0300

    index html added

--- src/main/java/com/kursatdev/cultfilms/controller/CultMoviesController.java
+++ src/main/java/com/kursatdev/cultfilms/controller/CultMoviesController.java
@@ -2,6 +2,7 @@ package com.kursatdev.cultfilms.controller;
 
 import com.kursatdev.cultfilms.model.Category;
 import com.kursatdev.cultfilms.model.Movie;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
